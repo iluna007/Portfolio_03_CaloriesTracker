554369f52e2c755954a8a0594aaeacb1c5af2be3
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-10-02 23:57:52 +0100

    add delete buttoms

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useReducer, useEffect, useMemo } from "react"
 import Form from "./components/Form"
 import { activityReducer, initalSatate } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList"
@@ -7,6 +7,11 @@ function App() {
 
   const [state, dispatch] = useReducer(activityReducer, initalSatate)
 
+  useEffect(() => {
+    localStorage.setItem('activities', JSON.stringify(state.activities))
+  }, [state.activities])
+
+  const canRestartApp = () => useMemo(() => state.activities.length > 0, [state.activities])
 
   return (
     <>
@@ -15,18 +20,27 @@ function App() {
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Calories counter
           </h1>
+          <button 
+            className="text-white bg-gray-400 px-5 py-2 rounded-lg disabled:opacity-10"
+            onClick={() => dispatch({type: 'restart-app'})}
+            disabled={!canRestartApp()}   
+          > 
+            Restart 
+          </button>
         </div>
       </header>
       <section className="bg-gray-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form 
             dispatch={dispatch}
+            state={state}
           />
         </div>
       </section>
       <section className="p-10 mx-auto max-w-4xl">
         <ActivityList 
-          activities={state.activities}
+          activities={state?.activities ?? []}
+          dispatch={dispatch}
         />
 
       </section>    
--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,40 +1,68 @@
+import { useMemo, Dispatch } from 'react'
 import { categories } from "../data/categories"
 import { Activity } from "../types"
-import { useMemo } from 'react'
+import { PencilSquareIcon } from "@heroicons/react/24/outline"
+import { TrashIcon } from "@heroicons/react/24/outline"
+import { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
     activities: Activity[]
+    dispatch: Dispatch<ActivityActions>
 }
 
-export default function ActivityList({activities} : ActivityListProps) {
+export default function ActivityList({activities, dispatch } : ActivityListProps) {
 
     const categoryName = useMemo(() => 
         (category: Activity['category']) => categories.map( cat => cat.id.toString() === category ? cat.name : '')
     , [activities])
+
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
 
     return (
         <>
             <h2 className="text-4xl font-bold text-slate-600 text-center">
                 Food and Activities
             </h2>
-            {
-               activities.map(activity => (
-                     <div
-                          key={activity.id}
-                          className="bg-white p-5 rounded-lg shadow-md mt-5"
+
+            { isEmptyActivities ? 
+            <p className='text-center my-5'>No entries</p> :
+            
+            activities.map(activity => (
+                     <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex
+                     justify-between items-center rounded-lg shadow-md"
                      >    
-                         <p>
-                            {categoryName(activity.category)}
-                          </p>
-                          <h3 className="text-2xl font-semibold text-slate-600">
-                            {activity.name}
-                          </h3>
-                          <p className="text-lg text-slate-500">
-                            {activity.calories} calories
-                          </p>
+                        <div className="space-y-2 relative">
+                            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase
+                                font-bold
+                                ${activity.category.toString() === '1' ? 'bg-gray-400' : 'bg-slate-800'}`}>
+                                {categoryName(activity.category)}
+                            </p>
+                            <p className="text-2xl font-semibold text-slate-600">
+                                {activity.name}
+                            </p>
+                            <p className="text-lg text-slate-500">
+                                {activity.calories} calories
+                            </p>
+                        </div>
+
+                        <div className="flex gap-5 items-center">
+                            <button 
+                                className="text-slate-500 hover:text-slate-700"
+                                onClick={()=>dispatch({type : "set-activeId", payload: {id: activity.id}})}>
+                                
+                                <PencilSquareIcon className="h-5 w-5"/>
+                            </button>
+                            <button 
+                                className="text-slate-300 hover:text-slate-700"
+                                onClick={()=>dispatch({type : "delete-activity", payload: {id: activity.id}})}>
+                                
+                                <TrashIcon className="h-5 w-5"/>
+                            </button>
+                        </div>
+                            
                      </div>
-                ))
-            }
+                ))}
         </>
     )
 }
+      
